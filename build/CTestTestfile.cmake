f71d9101f5aab62cc14f7edd1dadfe399d66f803
ChenChao_GitCode <ChenChao_GitCode@noreply.gitcode.com>
2026-01-22 14:45:28 +0800

    类型系统

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ChenChao/Documents/gitcode/CN_Language
-# Build directory: C:/Users/ChenChao/Documents/gitcode/CN_Language/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
